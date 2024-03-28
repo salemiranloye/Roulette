c4387d2572a7258667f6d1d49ba0844db6c92393
Salem Iranloye <salemiranloye@gmail.com>
2024-03-28 12:41:44 -0500

    IOS & Windows Compaitblity

--- main.c
+++ main.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
-#include <windows.h>
 
 #define startingBalance 500
 
 void welcomeUser(char username[50]) {
-    DWORD usernameSize = sizeof(char) * 50;
-
-    if (!GetUserName(username, &usernameSize)) {
-        printf("Please enter your username: ");
-        scanf("%s", username);
-    }
+    printf("Please enter your username: ");
+    scanf("%s", username);
     printf("Welcome %s to Roulette\n", username);
 }
 
 int main() {
     char username[50];
     int balance = startingBalance;
-    welcomeUser(username);
-    
-
+    welcomeUser(&username);
     return 0;
 }
